e0237153a87124135a75a4a729e3a362efde8ca6
Noisy-GH <felipemonteirodata@gmail.com>
2026-05-28 13:29:37 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,6 +7,17 @@ TABLES CREATION
 Script Purpose:
 Criar tables, tomando o cuidado de observar antes se já existe alguma com o mesmo nome.
 Se já existe, dropamos.
+
+Naming:
+nomedatabase.nivelcor_source_nometabela
+
+Exemplo:
+DataWareHouse.bronze_crm_cust_info
+
+nomedatabase = DataWareHouse
+nivelcor = bronze
+source = crm
+nometabela = cust_info
 =========================================================
 Notas:
 ATENÇÃO PARA OS TIPOS E A NOMENCLATURA
@@ -14,23 +25,22 @@ COLOCAR OS NOMES DAS SOURCES NO COMEÇO DE CADA TABLE APÓS O NOME DO DATABASE (
 =========================================================
 */
 
-DROP TABLE if EXISTS DataWareHouse.crm_cust_info;
+DROP TABLE if EXISTS DataWareHouse.bronze_crm_cust_info;
 
-CREATE TABLE DataWareHouse.crm_cust_info (
+CREATE TABLE DataWareHouse.bronze_crm_cust_info (
 cst_id int,
 cst_key varchar(50),
 cst_firstname varchar(50),
 cst_lastname varchar(50),
 cst_marital_status varchar(50),
 cst_gndr varchar(50),
-cst_create_date datetime
+cst_create_date datetime      
 );
 
-select * from datawarehouse.crm_cust_info;
 
-DROP TABLE if EXISTS DataWareHouse.crm_prd_info;
+DROP TABLE if EXISTS DataWareHouse.bronze_crm_prd_info;
 
-CREATE TABLE DataWareHouse.crm_prd_info (
+CREATE TABLE DataWareHouse.bronze_crm_prd_info (
 prd_id int,
 prd_key varchar(50),
 prd_nm varchar(50),
@@ -41,9 +51,9 @@ prd_end_dt datetime
 );
 
 
-DROP TABLE if EXISTS DataWareHouse.crm_sales_details;
+DROP TABLE if EXISTS DataWareHouse.bronze_crm_sales_details;
 
-CREATE TABLE DataWareHouse.crm_sales_details (
+CREATE TABLE DataWareHouse.bronze_crm_sales_details (
 sls_ord_num VARCHAR(50),
 sls_prd_key VARCHAR(50),
 sls_cust_id INT,
@@ -56,24 +66,24 @@ sls_price int
 );
 
 
-DROP TABLE if EXISTS DataWareHouse.erp_cust_az12;
+DROP TABLE if EXISTS DataWareHouse.bronze_erp_cust_az12;
 
-CREATE TABLE DataWareHouse.erp_cust_az12 (
+CREATE TABLE DataWareHouse.bronze_erp_cust_az12 (
 CID VARCHAR(50),
 BDATE DATE,
 GEN VARCHAR(50)
 );
 
-DROP TABLE if EXISTS DataWareHouse.erp_loc_a101;
+DROP TABLE if EXISTS DataWareHouse.bronze_erp_loc_a101;
 
-CREATE TABLE DataWareHouse.erp_loc_a101 (
+CREATE TABLE DataWareHouse.bronze_erp_loc_a101 (
 CID VARCHAR(50),
 CNTRY VARCHAR(50)
 );
 
-DROP TABLE if EXISTS DataWareHouse.erp_px_cat_g1v2;
+DROP TABLE if EXISTS DataWareHouse.bronze_erp_px_cat_g1v2;
 
-CREATE TABLE DataWareHouse.erp_px_cat_g1v2 (
+CREATE TABLE DataWareHouse.bronze_erp_px_cat_g1v2 (
 ID VARCHAR(50),
 CAT VARCHAR(50),
 SUBCAT VARCHAR(50),
